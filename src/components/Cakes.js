ce05ac5fe2b7b729e29f912be454acf90d841cce
Arash Dai <arashdai@gmail.com>
2015-10-18 13:35:32 -0700

    reattempting push

--- src/components/Cakes.js
+++ src/components/Cakes.js
@@ -9,58 +9,36 @@ var Cakes = React.createClass({
         <div className="navbar" >
           <ul className="nav nav-tabs" id="cakeNav">
             
-              <li type="button" className="navbar-toggle collapsed active" data-toggle="collapse" data-target="#defaultcakeBody" aria-expanded="true" aria-controls="navbar"role="presentation">
+              <li role="presentation" className="active">
               <a>Cakes</a>
               </li>
             
             
-              <li type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#weddingBody" aria-expanded="false" aria-controls="navbar" role="presentation">
+              <li role="presentation">
               <a>Weddings</a>
               </li>
             
             
-              <li type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bdayBody" aria-expanded="false" aria-controls="navbar" role="presentation">
+              <li role="presentation">
               <a>Birthday</a>
               </li>            
             
-              <li type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#eventBody" aria-expanded="false" aria-controls="navbar" role="presentation">
+              <li role="presentation">
               <a>Events</a>
               </li>
             
           </ul>
         </div>
 
-        <div id="defaultcakeBody" className="navbar-collapse collapse" aria-expanded="true">
+        <div id="weddingBody">
 
           <div id="cakeBlurb" className="col-xs-12">
-            text about our instore cakes will go here
-          </div>
-
-        </div>
-
-        <div id="weddingBody" className="navbar-collapse collapse" aria-expanded="false">
-
-          <div id="cakeBlurb" className="col-xs-12">
-            text about our wedding cakes will go here
+          text about our instore cakes will go here
           </div>
 
         </div>
         
-        <div id="bdayBody" className="navbar-collapse collapse" aria-expanded="false">
-
-          <div id="cakeBlurb" className="col-xs-12">
-            text about our birthday cakes will go here
-          </div>
-
-        </div>
-        
-        <div id="eventBody" className="navbar-collapse collapse" aria-expanded="false">
-
-          <div id="cakeBlurb" className="col-xs-12">
-            text about our event cakes will go here
-          </div>
-
-        </div>
+      
 
 
       </div>
