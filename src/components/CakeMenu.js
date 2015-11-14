14f53d7b4e1e110109842726dccdc65b2b28d066
Arash Dai <arashdai@gmail.com>
2015-11-14 00:59:52 -0800

    cake menu

--- src/components/CakeMenu.js
+++ src/components/CakeMenu.js
@@ -6,16 +6,23 @@ var CakeMenu = React.createClass({
     return(
       <div id='card' className='container-fluid'>
         <h1 className='text-center'>Cakes Menu</h1>
-          <h3 className='text-center'>Instore Cakes</h3>
+          <h3 className='text-center'>In-Store Cakes</h3>
+
+          <p className='text-center'>
+            A variety of these cakes are always avalible in the bakery
+          </p>
 
           <div id='CakeMenuItem'>
             <img className="col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
             
               <h2>Sacher Torte</h2>
             
+              <p>
+               8 inch round
+              </p>
                
               <p className="col-sm-offset-2" id='MenuDesc'>
-                description here
+                Layers of chocolate cake, filled with raspberry ,and chocolate mousse, coated with dark chocolate ganache
               </p>
                             
 
@@ -24,40 +31,239 @@ var CakeMenu = React.createClass({
               </p>
 
           </div>
-          <br></br>
+            <br></br>
           <div id='CakeMenuItem'>
             <img className="col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
             
             <h2>Parisian Torte</h2>
 
-            <p className='col-sm-offset-1'>  
-              description here
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
+              Layers of white cake with raspberry and bavarian cream filling, coated with white chocolate ganache 
             </p>
 
             <p className='col-sm-offset-11'>price here</p>
 
           </div>
-          <br></br>
+            <br></br>
           <div id='CakeMenuItem'>
             <img className="col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
             
             <h2>Shadow Torte</h2>
 
-            <p className='col-sm-offset-1'>  
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
+              Half white cake, half chocolate with fudge filling, frosted with vanilla buttercream with chocolate ganache dripping down the sides
+            </p>
+
+            <p className='col-sm-offset-11'>price here</p>
+
+          </div>
+            <br></br>
+          <div id='CakeMenuItem'>
+            <img className=" col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
+            
+            <h2> Carrot Cake</h2>
+
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
+             description here
+            </p>
+
+            <p className='col-sm-offset-11'>price here</p>
+
+          </div>
+            <br></br>
+          <div id='CakeMenuItem'>
+            <img className=" col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
+            
+            <h2> Fudge Torte</h2>
+
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
               description here
             </p>
 
             <p className='col-sm-offset-11'>price here</p>
 
           </div>
-          <br></br>
+            <br></br>
+          <div id='CakeMenuItem'>
+            <img className=" col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
+            
+            <h2> Rev Velvet Cake</h2>
+
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
+              description here
+            </p>
+
+            <p className='col-sm-offset-11'>price here</p>
+
+          </div>
+            <br></br>
+          <div id='CakeMenuItem'>
+            <img className=" col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
+            
+            <h2> Bannana Cake</h2>
+
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
+              description here
+            </p>
+
+            <p className='col-sm-offset-11'>price here</p>
+
+          </div>
+            <br></br>
+          <div id='CakeMenuItem'>
+            <img className=" col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
+            
+            <h2> German Chocolate Cake</h2>
+
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
+              description here
+            </p>
+
+            <p className='col-sm-offset-11'>price here</p>
+
+          </div>
+            <br></br>
+          <div id='CakeMenuItem'>
+            <img className=" col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
+            
+            <h2> Confetti Birthday Cake</h2>
+
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
+              description here
+            </p>
+
+            <p className='col-sm-offset-11'>price here</p>
+
+          </div>
+            <br></br>
+          <div id='CakeMenuItem'>
+            <img className=" col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
+            
+            <h2> Cheesecake </h2>
+
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
+              description here
+            </p>
+
+            <p className='col-sm-offset-11'>price here</p>
+
+          </div>
+            <br></br>
+          <div id='CakeMenuItem'>
+            <img className=" col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
+            
+            <h2> Chocolate Chip Cheesecake</h2>
+
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
+              description here
+            </p>
+
+            <p className='col-sm-offset-11'>price here</p>
+
+          </div>
+            <br></br>
+          <div id='CakeMenuItem'>
+            <img className=" col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
+            
+            <h2> Strawberry Cheesecake</h2>
+
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
+              description here
+            </p>
+
+            <p className='col-sm-offset-11'>price here</p>
+
+          </div>
+            <br></br>
+          <div id='CakeMenuItem'>
+            <img className=" col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
+            
+            <h2> Strawberry Boston Torte</h2>
+
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
+              description here
+            </p>
+
+            <p className='col-sm-offset-11'>price here</p>
+
+          </div>
+            <br></br>
+          <div id='CakeMenuItem'>
+            <img className=" col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
+            
+            <h2> Blackforest Cake</h2>
+
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
+              description here
+            </p>
+
+            <p className='col-sm-offset-11'>price here</p>
+
+          </div>
+            <br></br>
           <div id='CakeMenuItem'>
             <img className=" col-sm-2 img-circle" src="./assets/CS-080.jpg"/>
             
             <h2> Carrot Cake</h2>
 
-            <p className='col-sm-offset-1'>  
-             8in round description here
+            <p>
+              8 inch round
+            </p>
+
+            <p className='col-sm-offset-2' id='MenuDesc'>  
+              description here
             </p>
 
             <p className='col-sm-offset-11'>price here</p>
