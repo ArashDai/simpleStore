d8d0f61f09fa60008c24a517a1dda073e49a5607
Arash Dai <arashdai@gmail.com>
2015-11-16 00:19:27 -0800

    starting order form

---
+++ src/components/CustomCake.js
@@ -0,0 +1,26 @@
+var React =  require('react');
+var Link = require('react-router').Link;
+
+
+var CustomCake = React.createClass({
+  render:function(){
+    return(
+      <div id='card' className='container-fluid'>
+        <h1 className='text-center'>Custom Cake Creator</h1>
+        <p>The perfect cake awaits</p>
+        <form>
+
+
+
+
+
+        </form>
+
+
+
+      </div>
+      )}
+  });
+
+
+module.exports = CustomCake;
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,7 +9,8 @@ var Intro = React.createClass({
           <img id="introImg" className="img-responsive" src="/assets/diningroom.jpg" />
         </div>
 
-        <p className='row col-sm-10 col-sm-offset-1' id="introBlurb">  Lorem ipsum dolor sit amet, mauris tincidunt tellus nisl mattis luctus, tempor nullam, et aenean congue, nullam aliquam, at ante. Vivamus suspendisse ut nec proin in et, cursus eu taciti vitae cum, facilisis nascetur libero platea vitae libero, sodales lacinia non turpis eu wisi, vestibulum aenean habitant aliquam vestibulum maecenas. Ultrices ullam metus ante, congue eos dui condimentum dui turpis in, vulputate donec ad aliquam eget massa pede.</p>
+        <p className='row col-sm-10 col-sm-offset-1' id="introBlurb">  Lorem ipsum dolor sit amet, mauris tincidunt tellus nisl mattis luctus, tempor nullam, et aenean congue, nullam aliquam, at ante. Vivamus suspendisse ut nec proin in et, cursus eu taciti vitae cum, facilisis nascetur libero platea vitae libero, sodales lacinia non turpis eu wisi, vestibulum aenean habitant aliquam vestibulum maecenas. Ultrices ullam metus ante, congue eos dui condimentum dui turpis in, vulputate donec ad aliquam eget massa pede.
+        </p>
 
       </div>
 
